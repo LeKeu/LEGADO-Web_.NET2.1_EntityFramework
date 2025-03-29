e4c8004e6cc8c5aa1eee947a9779ec72db5d5b1f
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-28 22:59:45 -0300

    seller class

---
+++ Sales-Web-MVC/Models/Seller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales_Web_MVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+    }
+}
