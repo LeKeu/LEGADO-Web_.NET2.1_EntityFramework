c70800a925a65f597e413854a4645e77bd89a240
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-28 23:52:55 -0300

    department constructors

--- Sales-Web-MVC/Models/Department.cs
+++ Sales-Web-MVC/Models/Department.cs
@@ -10,5 +10,13 @@ namespace Sales_Web_MVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
