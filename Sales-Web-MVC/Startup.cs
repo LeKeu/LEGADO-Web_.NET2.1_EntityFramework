7764766b05fae52fd2fb58848ad296a1fd283cf3
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-30 16:17:42 -0300

    especificando a localizacao do app

--- Sales-Web-MVC/Startup.cs
+++ Sales-Web-MVC/Startup.cs
@@ -2,12 +2,15 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Sales_Web_MVC.Models;
 using Sales_Web_MVC.Data;
 using Sales_Web_MVC.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace Sales_Web_MVC
 {
@@ -45,6 +48,16 @@ namespace Sales_Web_MVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localuzationOption = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localuzationOption);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
